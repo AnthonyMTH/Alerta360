cd608008e8c8a51dcb0e821908eedebdf01234b2
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2025-05-20 19:39:29 -0500

    feat: add bottom navigation

---
+++ app/src/main/java/com/unsa/alerta360/presentation/account/AccountScreen.kt
@@ -0,0 +1,12 @@
+package com.unsa.alerta360.presentation.account
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AccountScreen() {
+    Column {
+        Text("AccountScreen")
+    }
+}
---
+++ app/src/main/java/com/unsa/alerta360/presentation/map/MapScreen.kt
@@ -0,0 +1,12 @@
+package com.unsa.alerta360.presentation.map
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MapScreen() {
+    Column {
+        Text("MapScreen")
+    }
+}
